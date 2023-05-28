967548f660ee3fda1d9ca448282bb72b42695a6c
NEIL-PC <NEIL-PC@DESKTOP-SSU2LH6>
2023-05-28 11:28:33 +0800

    Add project files.

---
+++ Water Drinking Log/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Water_Drinking_Log.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
