5830a60582790c11aa5c02824e858be3724125c3
NEIL-PC <NEIL-PC@DESKTOP-SSU2LH6>
2023-06-04 15:06:09 +0800

    update the program

---
+++ Water Drinking Log/Models/DrinkingWaterLog.cs
@@ -0,0 +1,9 @@
+﻿namespace Water_Drinking_Log.Models
+{
+    public class DrinkingWaterLog
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }  
+        public int Quantity { get; set; }
+    }
+}
